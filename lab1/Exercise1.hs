1357dc0bdb97b670d35921e75d1767ecebe51bcd
Viciooo <piotr.witek@spoton.com>
2024-09-24 23:13:33 +0200

    ex1 wip

--- lab1/Exercise1.hs
+++ lab1/Exercise1.hs
@@ -17,12 +17,46 @@ isDevidedProperty = forAll genPositiveInt (\x -> isDevided (factorial x) x)
 factorialDefinition :: Property
 factorialDefinition = forAll genPositiveInt (\x -> factorial x == product [1..x])
 
+
+prop_multiplicative :: Property
+prop_multiplicative = forAll nonNegativeNumberPair $ \(n,m) ->
+    n < m ==>
+    let left = factorial m
+        right = factorial n * product [n+1..m]
+    in left == right
+
+
+prop_logarythmic :: Property
+-- it will have issue due to floating point precision
+-- to mitigate this we will have to introduce an epsilon value
+prop_logarythmic = forAll nonNegativeNumber $ \n ->
+    let left = log (fromIntegral (factorial n))
+        right = sum [log (fromIntegral x) | x <- [1..n]]
+    in left == right
+
+prop_logarythmic' :: Property
+-- fixed by introducing epsilon
+prop_logarythmic' = forAll nonNegativeNumber $ \n ->
+    let epsilon = 0.0001
+        left = log (fromIntegral (factorial n))
+        right = sum [log (fromIntegral x) | x <- [1..n]]
+    in abs (left - right) < epsilon
+
+
 main :: IO ()
 main =  do
+    -- properties were taken from https://en.wikipedia.org/wiki/Factorial
+
     print "=== Divide property ==="
     quickCheck isDevidedProperty 
     print "=== Definition property ==="
-    quickCheck factorialDefinition 
+    quickCheck factorialDefinition
+    print "=== Multiplicative property ==="
+    quickCheck prop_multiplicative
+    print "=== Logarithmic property with floating point precision issue ==="
+    quickCheck prop_logarythmic
+    print "=== Logarithmic property ==="
+    quickCheck prop_logarythmic'
 
 -- Test report: Function was tested on two properties: 
     -- 1. Is the result divisible by every number from 1 to x:
@@ -31,10 +65,14 @@ main =  do
     -- 2. Is the result equal to the number calculated using definition of factorial (without using recursion)
     -- The result should match the factorial calculated by the non-recursive definition
 
+    -- 3. Multiplicative property:
+    -- The factorial of m should be equal to the factorial of n multiplied by the product of all numbers between n and m.
+
+    -- 4. Logarithmic property:
+    -- The logarithm of the factorial of n should be equal to the sum of the logarithms of all numbers between 1 and n.
+
     -- Properties where tested using randomly generated numbers with QuickCheck's Gen.
     -- There was contraint applied onto number genetrator: generated numbers are only non negative integers
     -- This contraint was created becuse factorial by definiton is applied to non negative integers
-
-    -- All the tested passed
 
 -- Time Spent: 30 min
