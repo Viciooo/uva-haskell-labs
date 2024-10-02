63b4226b4707392fd650059625b36702b33e89e7
Rodrigo Gonçalves <r0drigo.dev@icloud.com>
2024-10-03 00:24:45 +0200

    1-3

---
+++ lab3/Exercise2.hs
@@ -0,0 +1,91 @@
+-- Time Spent: 120 min
+module Exercise2 where
+
+import Exercise1
+import SetOrd
+import Test.QuickCheck
+
+-- import Control.Applicative (Alternative(some))
+
+exposeSet :: Set a -> [a]
+exposeSet (Set s) = s
+
+-- Return a set with Ord a => Set a -> Set a -> Set a elements that are present in booth sets
+setIntersection :: Ord a => Set a -> Set a -> Set a
+setIntersection (Set s1) (Set s2) = list2set $ filter (`elem` s2) s1
+
+-- Union is the merger of the two sets
+setUnion :: Ord a => Set a -> Set a -> Set a
+setUnion (Set s1) (Set s2) = list2set (s1 ++ s2)
+
+-- The difference between two sets is the elements of the intersection that don't belong to the union
+setDifference :: Ord a => Set a -> Set a -> Set a
+setDifference s1 s2 = do
+    let intersectionList = exposeSet $ setIntersection s1 s2
+    let unionList = exposeSet $ setUnion s1 s2
+
+    list2set $ filter (`notElem` intersectionList) unionList
+
+
+-- Properties
+
+-- Check if all elements of the intersection Set are part of both original Sets
+intersectionProperty :: Ord a => Set a -> Set a -> Set a -> Bool
+intersectionProperty (Set sInt) (Set s1) (Set s2) =  and [ x `elem` s1 && x `elem` s2  | x <- sInt]
+
+-- Check if all elements of the original Sets are part of the union Set
+unionProperty :: Ord a => Set a -> Set a -> Set a -> Bool
+unionProperty (Set sUn) (Set s1) (Set s2) =  and ( [ x `elem` sUn | x <- s1] ++ [ x `elem` sUn | x <- s2])
+
+-- Check if all elements of the difference Set are part of one and only one of the original Sets
+differenceProperty :: Ord a => Set a -> Set a -> Set a -> Bool
+differenceProperty (Set sDiff) (Set s1) (Set s2) =  and [ (x `elem` s1 || x `elem` s2) && not (x `elem` s1 && x `elem` s2)  | x <- sDiff]
+
+--Tests
+testIntersection :: (Set Int, Set Int) -> Bool
+testIntersection (s1,s2) = do
+    let s3 = setIntersection s1 s2
+    intersectionProperty s3 s1 s2
+
+testUnion:: (Set Int, Set Int) -> Bool
+testUnion (s1,s2)  = do
+    let s3 = setUnion s1 s2
+    unionProperty s3 s1 s2
+
+testDifference :: (Set Int, Set Int) -> Bool
+testDifference (s1,s2)  = do
+    let s3 = setDifference s1 s2
+    differenceProperty s3 s1 s2
+        
+
+quicChec :: ((Set Int, Set Int) -> Bool) -> Int -> IO Bool
+quicChec fun 0 = return True
+quicChec fun n = do
+    s1 <- randomSet
+    s2 <- randomSet
+    
+    next <- quicChec fun (n-1)
+        
+    return $ fun (s1,s2) && next
+
+testAll :: IO ()
+testAll = do
+    putStrLn "Using Scratch Generator"
+    putStr "Intersection="
+    t1 <- quicChec testIntersection 100
+    if t1 then putStrLn "Pass" else putStrLn "Fail"
+    putStr "Union="
+    t2 <- quicChec testUnion 100
+    if t2 then putStrLn "Pass" else putStrLn "Fail"
+    putStr "Difference="
+    t3 <- quicChec testDifference 100
+    if t3 then putStrLn "Pass" else putStrLn "Fail"
+
+    -- Use the instance Arbitrary predefined previously
+    putStrLn "Using QuickCheck Generator"
+    putStr "Intersection="
+    quickCheck testIntersection
+    putStr "Union="
+    quickCheck testUnion
+    putStr "Difference="
+    quickCheck testDifference
---
+++ lab3/Exercise3.hs
@@ -0,0 +1,7 @@
+module Exercise3 where
+import Data.List
+
+type Rel a = [(a,a)]
+
+symClos :: Ord a => Rel a -> Rel a
+symClos xs = sort $ nub $ xs ++ [ (x2,x1) | (x1, x2) <- xs ]
